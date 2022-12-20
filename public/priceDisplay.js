d15d51fab3751c8d55738397302f00a8af56ec66
Styrbjörn Nordberg <styris.n@gmail.com>
2022-12-20 18:28:04 +0100

    finnished priceDisplay.js

---
+++ public/priceDisplay.js
@@ -0,0 +1,13 @@
+function priceDisplay(){
+    fetch("./prices.json")
+    .then((Response) => Response.json())
+    .then(console.log('successfully loaded json'))
+    .then((prices) => {
+        var price = document.querySelector('#room');
+        var totalCost = document.querySelector('#totalCost')
+        var totalPrice = (prices["value"+price.value]);
+
+        totalCost.innerHTML = totalPrice;
+    });
+}
+priceDisplay();
