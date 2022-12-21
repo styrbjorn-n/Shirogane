7181d15d633be9fc8cf2c1872922ce36c2a792cb
Styrbjörn Nordberg <styris.n@gmail.com>
2022-12-21 16:17:16 +0100

    created admin site

---
+++ public/admin.js
@@ -0,0 +1,18 @@
+function changePrices(){
+
+}
+function displayCurentPrice(){
+    fetch('./prices.json')
+    .then((Response) => Response.json())
+    .then(console.log('successfully loaded json'))
+    .then((prices) => {
+        var priceDisplays = document.querySelectorAll('.priceDisplay');
+        var i = 1;
+        priceDisplays.forEach((display) => {
+            display.innerHTML = prices['value'+i];
+            i++;
+        })
+        console.log('successfully printed json');
+    });
+}
+displayCurentPrice();
