e6aaabca0eea2f795672612ff85b07cabfb75042
Styrbjörn Nordberg <styris.n@gmail.com>
2022-12-21 22:09:11 +0100

    fixed admin page

---
+++ public/changePrices.php
@@ -0,0 +1,22 @@
+<?php
+function changePrices()
+{
+    $prices = $_POST;
+    $i = 0;
+    foreach ($prices as $price) {
+        $i++;
+        if ($price != null) {
+            if (is_numeric($price)) {
+                $jsonString = file_get_contents('prices.json');
+                $data = json_decode($jsonString, true);
+                echo ($data['value' . $i] . '<br>');
+                echo ($price . '<br>');
+
+                $data['value' . $i] = $price;
+                $newJsonString = json_encode($data);
+                file_put_contents('prices.json', $newJsonString);
+            }
+        }
+    }
+}
+changePrices();
